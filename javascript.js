399dea2d8938a5057790016bb814e77ffe26d3de
ErnestoC <ernesto.cabello.v@gmail.com>
2021-05-04 23:27:19 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+// Gets the video src from the data-src on each button
+
+var $videoSrc;  
+$('.video-btn').click(function() {
+    $videoSrc = $(this).data( "src" );
+});
+console.log($videoSrc);
+
+    
+    
+// Esto es lo que hace que se autoreproduzca al abrir el modal
+$('#myModal').on('shown.bs.modal', function (e) {
+    
+// Esto pone el video en autoplay y que no muestre videos relacionados (los videos que aparecen en la barra de abajo)
+$("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
+})
+    
+
+
+// Esto deberia pausar el video completamente al cerrar el modal
+$('#myModal').on('hide.bs.modal', function (e) {
+    $("#video").attr('src',$videoSrc); 
+}) 
+    
+    
+
+});
